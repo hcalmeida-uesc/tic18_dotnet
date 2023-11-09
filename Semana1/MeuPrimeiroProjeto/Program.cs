a5ebe515876f315cb1b24c2627f5b186b1f1034e
Hélder Almeida <hcalmeida@uesc.br>
2023-11-09 17:35:36 -0300

    Fim de aula 1

--- Semana1/MeuPrimeiroProjeto/Program.cs
+++ Semana1/MeuPrimeiroProjeto/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+#region  Teste de Tipos de Dados
+   int tipoInteiro;
+   double tipoDouble;
+   string tipoString;
+   long tipoLong;
+
+   tipoInteiro = int.MaxValue;
+   tipoLong = tipoInteiro;
+  // tipoInteiro = tipoLong;
+
+   tipoString = "100A";
+
+   tipoInteiro = (int)(tipoString);
+
+   Console.WriteLine("O máximo inteiro é "+tipoInteiro);
+
+   Console.WriteLine("O máximo long é "+tipoLong);
+#endregion
+
+#region Operadores
+   tipoDouble = tipoInteiro + tipoLong;
+   tipoInteiro = tipoDouble + tipoLong;
+   tipoLong = tipoDouble + tipoInteiro; 
+
+   tipoInteiro = 10 > 5 ? 1 : 0;
+#endregion
