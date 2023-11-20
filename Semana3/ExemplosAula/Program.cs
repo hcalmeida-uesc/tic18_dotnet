c5d2b122ea548c832859f6d6dd9b567fc4236b6d
Hélder Almeida <hcalmeida@uesc.br>
2023-11-20 15:32:43 -0300

    Refactor LambdaExpression and Linq examples

--- Semana3/ExemplosAula/Program.cs
+++ Semana3/ExemplosAula/Program.cs
@@ -27,7 +27,8 @@ list.ForEach(x => Console.WriteLine($"Tuple 4: {x.id}, {x.name}, {x.born.ToShort
 
 #endregion */
 
-#region LambdaExpression Examples
+/* #region LambdaExpression Examples
+
 Func<int, int, int> sum = (x, y) => x + y;
 Console.WriteLine($"Sum: {sum(10, 20)}");
 // Sum: 30
@@ -47,16 +48,22 @@ greet(person);
 Func<string, int, string> isBiggerThan = (string s, int x) => s.Length > x ? "Yes" : "No";
 var size = 5;
 Console.WriteLine($"The text {person} has more than {size} chars? {isBiggerThan(person, size)}");
-#endregion
 
-/* #region Linq Examples
+string[] people = { "Helder", "Nicole", "Gilvana" };
+char letter = 'N';
+Console.WriteLine($"People with name started with '{letter}': {string.Join(", ", people.Where(x => x.StartsWith(letter)))}");
+
+#endregion */
+
+#region Linq Examples
+
 List<int> list = new() { 1, 2, 3, 4, 5 };
 var squaredList = list.Select(x => x * x);
 Console.WriteLine($"Original List: {string.Join(", ", list)}");
 Console.WriteLine($"Squared  List: {string.Join(", ", squaredList)}");
 // Original List: 1, 2, 3, 4, 5
 // Squared  List: 1, 4, 9, 16, 25
-var summedList = list.Select((x,index) => sum(x, squaredList.ElementAt(index)));
+var summedList = list.Select((x,index) => x + squaredList.ElementAt(index));
 Console.WriteLine($"Summed   List: {string.Join(", ", summedList)}");
 // Summed   List: 2, 6, 12, 20, 30
 
@@ -66,4 +73,41 @@ listMultipleOfThree.AddRange(summedList.Where(x => x % 3 == 0).ToList());
 Console.WriteLine($"List Multiple of Three: {string.Join(", ", listMultipleOfThree)}");
 // List Multiple of Three: 3, 9, 6, 12, 30
 Console.WriteLine($"List Multiple of Three: {string.Join(", ", listMultipleOfThree.Order())}");
-#endregion */
+
+var students = new List<Student>{
+   new Student(1, "Helder Almeida", "123456789", new DateTime(1987, 9, 24), new List<string>{"123456789", "73988342986"}),
+   new Student(2, "Nicole Almeida", "987654321", new DateTime(2019, 1, 12), new List<string>{"123456789", "73987654321"}),
+   new Student(3, "Gilvana Rocha", "123456789", new DateTime(1984, 11, 24), new List<string>{"123456789", "77988237086"})
+};
+
+var any = students.Any();
+var anyHelder = students.Any(x => x.FullName.Contains("Helder"));
+//var single = students.Single(x => x.Id == 10);
+//throw exception
+var singleOrDefault = students.SingleOrDefault(x => x.Id == 10);
+
+var select = students.Select(x => x.PhoneNumbers);
+var selectMany = students.SelectMany(x => x.PhoneNumbers);
+
+var legalAge = students.Where(x => x.BirthDate <= DateTime.Today.AddYears(-18));
+
+Console.Read();
+
+public class Student{
+   public Student(int id, string fullName, string document, DateTime birthDate, List<string> phoneNumbers)
+   {
+      Id = id;
+      FullName = fullName;
+      Document = document;
+      BirthDate = birthDate;
+      PhoneNumbers = new List<string>(phoneNumbers);
+   }
+
+   public int Id { get; set; }
+   public string FullName { get; set; }
+   public string Document { get; set; }
+   public DateTime BirthDate { get; set; }
+   public List<string> PhoneNumbers { get; set; }
+}
+
+#endregion
