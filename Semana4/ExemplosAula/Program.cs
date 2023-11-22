52433e3ff34ad283f133bc27054bd8851ceedc1f
Hélder Almeida <hcalmeida@uesc.br>
2023-11-22 12:31:23 -0300

    Atualização dos exemplos

--- Semana4/ExemplosAula/Program.cs
+++ Semana4/ExemplosAula/Program.cs
@@ -5,8 +5,13 @@ CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
 
 List<Pessoa> people = App.AddPeople();
 
+Pessoa Valber = new Pessoa("Valber", "123456789-00", new DateTime(1990, 10, 10));
+
+people.Add(Valber);
+
 foreach (Pessoa person in people)
 {
+   Console.WriteLine($"ID: {person.Id}");
    Console.WriteLine($"Nome: {person.Name}");
    Console.WriteLine($"Documento: {person.Document}");
    Console.WriteLine($"Data de nascimento: {person.BirthDate.ToShortDateString()}");
