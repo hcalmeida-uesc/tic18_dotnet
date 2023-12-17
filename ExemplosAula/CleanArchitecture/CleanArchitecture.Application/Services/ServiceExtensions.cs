9ad60337d46e70b91cee3e7a9d1e8ef6a9d10129
Hélder Almeida <hcalmeida@uesc.br>
2023-12-17 16:26:43 -0300

    Add service extensions and validation behavior

---
+++ ExemplosAula/CleanArchitecture/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using CleanArchitecture.Application.Shared.Behavior;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application.Services;
+public static class ServiceExtensions
+{
+   public static void ConfigureApplicationApp(this IServiceCollection services)
+   {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+      services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+      services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+   }
+}
--- ExemplosAula/CleanArchitecture/CleanArchitecture.WebAPI/Program.cs
+++ ExemplosAula/CleanArchitecture/CleanArchitecture.WebAPI/Program.cs
@@ -1,25 +1,29 @@
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence;
+using CleanArchitecture.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigurePersistenceApp(builder.Configuration);
+builder.Services.ConfigureApplicationApp();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+CreateDatabase(app);
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
-
 app.Run();
+
+
+static void CreateDatabase(WebApplication app)
+{
+    var serviceScope = app.Services.CreateScope();
+    var dataContext = serviceScope.ServiceProvider.GetService<AppDbContext>();
+    dataContext?.Database.EnsureCreated();
+}
