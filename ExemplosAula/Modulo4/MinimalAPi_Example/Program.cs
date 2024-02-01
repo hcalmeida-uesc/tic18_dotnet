997e9a98988203792664de4bde2f9ffee45b5ecd
Hélder Almeida <hcalmeida@uesc.br>
2024-02-01 17:01:47 -0300

    Commented out HTTPS redirection and added a new response message

--- ExemplosAula/Modulo4/MinimalAPi_Example/Program.cs
+++ ExemplosAula/Modulo4/MinimalAPi_Example/Program.cs
@@ -3,10 +3,13 @@ using Modulo4.ExampleMiddleware;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseExampleMiddleware();
 
-//app.UseMiddleware<ExampleMiddleware>();
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello, World!\n");
+});
 
 app.Run();
