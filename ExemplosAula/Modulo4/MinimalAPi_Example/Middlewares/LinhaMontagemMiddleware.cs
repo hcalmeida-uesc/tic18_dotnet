a5ba631d8aecf284a34974a3c32cc15bdeca9461
Hélder Almeida <hcalmeida@uesc.br>
2024-02-02 01:00:45 -0300

    Add middleware functionality for adding chassi, motor, portas, pintura, and acabamento interno

--- ExemplosAula/Modulo4/MinimalAPi_Example/Middlewares/LinhaMontagemMiddleware.cs
+++ ExemplosAula/Modulo4/MinimalAPi_Example/Middlewares/LinhaMontagemMiddleware.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Modulo4.LinhaDeMontagem;
 public class AddChassiMiddleware
 {
@@ -8,8 +10,10 @@ public class AddChassiMiddleware
     }
     public async Task Invoke(HttpContext context, LinhaDeMontagemDescricao descricao)
     {
-        await context.Response.WriteAsync("Chassi adicionado\n");
+        context.Response.ContentType = "text/html; charset=utf-8";
+        descricao.AdicionarEtapa("Chassi", "Chassi adicionado");
         await _next(context);
+        await context.Response.WriteAsync(descricao.ToString());
     }
 }
 
@@ -20,9 +24,9 @@ public class AddMotorMiddleware
     {
         _next = next;
     }
-    public async Task Invoke(HttpContext context)
+    public async Task Invoke(HttpContext context, LinhaDeMontagemDescricao descricao)
     {
-        await context.Response.WriteAsync("Motor adicionado\n");
+        descricao.AdicionarEtapa("Motor", "Motor adicionado");
         await _next(context);
     }
 }
@@ -34,11 +38,13 @@ public class AddPortasMiddleware
     {
         _next = next;
     }
-    public async Task Invoke(HttpContext context)
+    public async Task Invoke(HttpContext context, LinhaDeMontagemDescricao descricao)
     {
-        await context.Response.WriteAsync("Portas adicionadas\n");
+        descricao.AdicionarEtapa("Portas", "Portas adicionadas");
         await _next(context);
+        descricao.AdicionarEtapa("Portas", $"Maçanetas {descricao.Cor} adicionadas");
     }
+
 }
 
 public class AddPinturaMiddleware
@@ -48,9 +54,11 @@ public class AddPinturaMiddleware
     {
         _next = next;
     }
-    public async Task Invoke(HttpContext context)
+    public async Task Invoke(HttpContext context, LinhaDeMontagemDescricao descricao)
     {
-        await context.Response.WriteAsync("Pintura adicionada\n");
+        var cores = new string[] { "Preto", "Branco", "Vermelho", "Azul" };
+        descricao.Cor = cores[new Random().Next(0, cores.Length)];
+        descricao.AdicionarEtapa("Pintura", $"Pintura adicionada na cor {descricao.Cor}");
         await _next(context);
     }
 }
@@ -63,7 +71,7 @@ public class AddInternoMiddleware
     }
     public async Task Invoke(HttpContext context, LinhaDeMontagemDescricao descricao)
     {
-        await context.Response.WriteAsync("Acabarmento interno adicionado\n");
+        descricao.AdicionarEtapa("Acabamento Interno", $"Acabamento Interno adicionado na cor {descricao.Cor}");
         if (!context.Response.HasStarted)
             await _next(context);
     }
@@ -71,5 +79,20 @@ public class AddInternoMiddleware
 
 public class LinhaDeMontagemDescricao
 {
-    List <string> descricao = new List<string>();
+    public List <(string,string)> descricao = new List<(string,string)>();
+    public string Cor { get; set; }
+    public void AdicionarEtapa(string etapa, string descricao)
+    {
+        this.descricao.Add((etapa, descricao));
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        foreach (var item in descricao)
+        {
+            sb.AppendLine($"Etapa {i++}: {item.Item1} - {item.Item2}<br>");
+        }
+        return sb.ToString();
+    }
 }
