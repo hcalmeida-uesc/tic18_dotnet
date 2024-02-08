0db3f5a45b2016349579b60b4d5d21e96c19be9f
Hélder Almeida <hcalmeida@uesc.br>
2024-02-07 21:29:32 -0300

    Add AuthService and IAuthService interfaces

---
+++ ExemplosAula/Modulo3/TechMed.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,47 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TechMed.Infrastructure.Auth;
+public class AuthService : IAuthService
+{
+   private readonly IConfiguration _configuration;
+
+   public AuthService(IConfiguration configuration)
+   {
+      _configuration = configuration;
+   }
+
+   public string GenerateJwtToken(string email, string role)
+   {
+      var issuer = _configuration["Jwt:Issuer"];
+      var audience = _configuration["Jwt:Audience"];
+      var key = _configuration["Jwt:Key"];
+      //cria uma chave utilizando criptografia simétrica
+      var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+      //cria as credenciais do token
+      var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+      
+      var claims = new[]
+      {
+         new Claim("userName", email),
+         new Claim(ClaimTypes.Role, role)
+      };
+
+      var token = new JwtSecurityToken( //cria o token
+         issuer: issuer, //emissor do token
+         audience: audience, //destinatário do token
+         claims: claims, //informações do usuário
+         expires: DateTime.Now.AddMinutes(30), //tempo de expiração do token
+         signingCredentials: credentials); //credenciais do token
+      
+
+      var tokenHandler = new JwtSecurityTokenHandler(); //cria um manipulador de token
+
+      var stringToken = tokenHandler.WriteToken(token);
+
+      return stringToken;
+   }
+}
---
+++ ExemplosAula/Modulo3/TechMed.Infrastructure/Auth/IAuthService.cs
@@ -0,0 +1,5 @@
+namespace TechMed.Infrastructure.Auth;
+public interface IAuthService
+{
+   string GenerateJwtToken(string email, string role); 
+}
