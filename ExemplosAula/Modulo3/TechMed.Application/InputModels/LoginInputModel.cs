191aa2cb9e1a505d84bfa57e740d094e8be6ed57
Hélder Almeida <hcalmeida@uesc.br>
2024-02-08 14:52:58 -0300

    Add LoginInputModel and LoginViewModel classes

---
+++ ExemplosAula/Modulo3/TechMed.Application/InputModels/LoginInputModel.cs
@@ -0,0 +1,6 @@
+namespace TechMed.Application.InputModels;
+public class LoginInputModel
+{
+   public required string Username { get; set; }
+   public required string Password { get; set; }
+}
---
+++ ExemplosAula/Modulo3/TechMed.Application/ViewModels/LoginViewModel.cs
@@ -0,0 +1,6 @@
+namespace TechMed.Application.ViewModels;
+public class LoginViewModel
+{
+   public required string Username { get; set; }
+   public required string Token { get; set; }
+}
