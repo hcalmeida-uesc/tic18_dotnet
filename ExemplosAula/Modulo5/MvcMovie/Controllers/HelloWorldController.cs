6f476114d4d246da5398ca0c6c0c259df4d1a26d
Hélder Almeida <hcalmeida@uesc.br>
2024-02-20 17:04:34 -0300

    Add HelloWorldController and corresponding views

---
+++ ExemplosAula/Modulo5/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/{name=Scott}&{numTimes=1}
+    public IActionResult Welcome(string name="World", int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
